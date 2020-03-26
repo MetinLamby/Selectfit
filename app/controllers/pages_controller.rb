910509bb2ab6c3f4e2209d5a3fdc8e9da8d536fa
Metin Lamby <metin.lamby@outlook.com>
2020-03-26 14:12:14 +0100

    worked on the landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
+    products = Product.all
+    @product_first = products[0]
+    @product_second = products[1]
+    @product_third = products[2]
+    @product_fourth = products[3]
   end
 
   def about
